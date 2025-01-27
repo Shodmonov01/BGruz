349b5624bfff80080c6a558614a5a9eeb08f4aa4
Shodmonov01 <a.shodmonov60@gmail.com>
2025-01-27 16:54:45 +0500

    UpDate 1lvl

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,55 +1,72 @@
-'use client';
-import DashboardNav from '@/components/shared/dashboard-nav';
-import { navItems } from '@/constants/data';
-import { useSidebar } from '@/hooks/use-sidebar';
-import { cn } from '@/lib/utils';
-import { ChevronsLeft } from 'lucide-react';
-import { useState } from 'react';
+'use client'
+import DashboardNav from '@/components/shared/dashboard-nav'
+import { navItems } from '@/constants/data'
+import { useSidebar } from '@/hooks/use-sidebar'
+import { cn } from '@/lib/utils'
+import { ChevronsLeft } from 'lucide-react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '/logoRb.png'
+import UserNav from './user-nav'
+import { ModeToggle } from './theme-toggle'
 
 type SidebarProps = {
-  className?: string;
-};
+    className?: string
+}
 
 export default function Sidebar({ className }: SidebarProps) {
-  const { isMinimized, toggle } = useSidebar();
-  const [status, setStatus] = useState(false);
+    const { isMinimized, toggle } = useSidebar()
+    const [status, setStatus] = useState(false)
 
-  const handleToggle = () => {
-    setStatus(true);
-    toggle();
-    setTimeout(() => setStatus(false), 500);
-  };
-  return (
-    <nav
-      className={cn(
-        `relative z-10 hidden h-screen flex-none  px-3 md:block`,
-        status && 'duration-500',
-        !isMinimized ? 'w-72' : 'w-[80px]',
-        className
-      )}
-    >
-      <div
-        className={cn(
-          'flex items-center px-0 py-5 md:px-2',
-          isMinimized ? 'justify-center ' : 'justify-between'
-        )}
-      >
-        {!isMinimized && <h1 className="text-2xl font-bold">Logo</h1>}
-        <ChevronsLeft
-          className={cn(
-            'size-8 cursor-pointer rounded-full border bg-background text-foreground',
-            isMinimized && 'rotate-180'
-          )}
-          onClick={handleToggle}
-        />
-      </div>
-      <div className="space-y-4 py-4">
-        <div className="px-2 py-2">
-          <div className="mt-3 space-y-1">
-            <DashboardNav items={navItems} />
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+    const handleToggle = () => {
+        setStatus(true)
+        toggle()
+        setTimeout(() => setStatus(false), 500)
+    }
+    return (
+        <nav
+            className={cn(
+                `relative z-10 hidden h-screen flex-none  px-3 md:block`,
+                status && 'duration-500',
+                !isMinimized ? 'w-72' : 'w-[80px]',
+                className
+            )}
+        >
+            <div
+                className={cn(
+                    'flex items-center px-0 py-5 md:px-2',
+                    isMinimized ? 'justify-center ' : 'justify-between'
+                )}
+            >
+                {!isMinimized && (
+                    <Link to={'/'} className='flex justify-center items-center gap-2'>
+                        <img src={logo} alt='' className='h-12' />
+                        <span>
+                            <span className='text-[#03b4e0]'>Биржа</span> Грузоверевозок
+                        </span>
+                    </Link>
+                )}
+                <ChevronsLeft
+                    className={cn(
+                        'size-8 cursor-pointer rounded-full border bg-background text-foreground',
+                        isMinimized && 'rotate-180'
+                    )}
+                    onClick={handleToggle}
+                />
+            </div>
+            <div className='space-y-4 py-4'>
+                <div className='px-2 py-2'>
+                    <div className='mt-3 space-y-1 flex justify-between'>
+                        <UserNav />
+                        <div className={isMinimized ? 'hidden' : 'inline-block'}>
+                            <ModeToggle />
+                        </div>
+                    </div>
+                    <div className='mt-3 space-y-1'>
+                        <DashboardNav items={navItems} />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
 }
--- src/components/shared/user-nav.tsx
+++ src/components/shared/user-nav.tsx
@@ -1,43 +1,41 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu';
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
 export default function UserNav() {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-14 w-14 rounded-full">
-          <Avatar className="h-14 w-14">
-            <AvatarImage
-              src={
-                'https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png'
-              }
-              alt={''}
-            />
-            <AvatarFallback>hello</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{'Admin'}</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {'admin@gmail.com'}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant='ghost' className='relative h-14 w-14 rounded-full'>
+                    <Avatar className='h-14 w-14'>
+                        <AvatarImage
+                            src={
+                                'https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png'
+                            }
+                            alt={''}
+                        />
+                        <AvatarFallback>hello</AvatarFallback>
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-56' align='end' forceMount>
+                <DropdownMenuLabel className='font-normal'>
+                    <div className='flex flex-col space-y-1'>
+                        <p className='text-sm font-medium leading-none'>{'Admin'}</p>
+                        <p className='text-xs leading-none text-muted-foreground'>{'admin@gmail.com'}</p>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {/* <DropdownMenuGroup>
           <DropdownMenuItem>
             Profile
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
@@ -51,13 +49,13 @@ export default function UserNav() {
             <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
           </DropdownMenuItem>
           <DropdownMenuItem>New Team</DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => console.log('logout')}>
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+        </DropdownMenuGroup> */}
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => console.log('logout')}>
+                    Log out
+                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
 }
